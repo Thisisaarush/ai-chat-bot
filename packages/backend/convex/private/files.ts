c86a6efca5bef6f43ba3344b6baabd7fb6133020
AarushTanwar <tanwaraarush007@gmail.com>
2025-09-04 14:43:01 +0530

    feat: enhance file handling with RAG integration; implement addFile and deleteFile mutations, improve metadata management, and update error handling

--- packages/backend/convex/private/files.ts
+++ packages/backend/convex/private/files.ts
@@ -1,12 +1,15 @@
-import { action } from "../_generated/server"
+import { action, mutation } from "../_generated/server"
 import { ConvexError, v } from "convex/values"
 import {
   contentHashFromArrayBuffer,
   guessMimeTypeFromContents,
   guessMimeTypeFromExtension,
+  vEntryId,
 } from "@convex-dev/rag"
 import { extractTextContent } from "../lib/extractTextContent"
 import rag from "../system/ai/rag"
+import { ar } from "zod/v4/locales"
+import { Id } from "../_generated/dataModel"
 
 const guessMimeType = (filename: string, bytes: ArrayBuffer): string => {
   return (
@@ -53,7 +56,7 @@ export const addFile = action({
       mimeType,
     })
 
-    const {} = await rag.add(ctx, {
+    const { entryId, created } = await rag.add(ctx, {
       namespace: orgId,
       text,
       key: filename,
@@ -64,6 +67,76 @@ export const addFile = action({
         category: category ?? null,
       },
       contentHash: await contentHashFromArrayBuffer(bytes),
+    })
+
+    if (!created) {
+      console.debug("Entry already exists, skipping upload metadata")
+      await ctx.storage.delete(storageId)
+    }
+
+    return {
+      url: await ctx.storage.getUrl(storageId),
+      entryId,
+    }
+  },
+})
+
+export const deleteFile = mutation({
+  args: {
+    entryId: vEntryId,
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+    if (identity === null) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "User not authenticated",
+      })
+    }
+
+    const orgId = identity.orgId as string
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "User not authorized",
+      })
+    }
+
+    const namespace = await rag.getNamespace(ctx, {
+      namespace: orgId,
+    })
+
+    if (!namespace) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Namespace not found",
+      })
+    }
+
+    const entry = await rag.getEntry(ctx, {
+      entryId: args.entryId,
+    })
+
+    if (!entry) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Entry not found",
+      })
+    }
+
+    if (entry.metadata?.uploadedBy !== orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "User not authorized to delete this entry",
+      })
+    }
+
+    if (entry.metadata?.storageId) {
+      await ctx.storage.delete(entry.metadata.storageId as Id<"_storage">)
+    }
+
+    await rag.deleteAsync(ctx, {
+      entryId: args.entryId,
     })
   },
 })
